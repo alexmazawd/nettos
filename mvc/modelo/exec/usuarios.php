b5cecc30009a26fc4eadc32b1663198aea0adfd6
alejoselopez <46781441+alejoselopez@users.noreply.github.com>
2019-03-06 21:11:57 +0100

    Create usuarios.php

---
+++ mvc/modelo/exec/usuarios.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daw2
+ * Date: 15/01/2019
+ * Time: 9:44
+ */
+
+
+class usuarios
+{
+
+    public static function existeUser($usuario) {
+
+        $database = medoo::getInstance();
+
+        $database->openConnection(MYSQL_CONFIG);
+
+        $datos = $database->select('usuarios', '*', ["usuario[=]" => $usuario]);
+
+        $database->closeConnection();
+
+        return $datos;
+    }
+
+    public static function searchUsuarioDB($usuario)
+    {
+        $database = medoo::getInstance();
+
+        $database->openConnection(MYSQL_CONFIG);
+
+        $datos = $database->select('usuarios', 'clave', [
+
+            "usuario[=]" => $usuario]);
+
+        $database->closeConnection();
+
+        if ($datos)
+
+            return $datos[0];
+
+        else
+
+            return $datos;
+    }
+
+    public static function duplicateUsuario($usuario)
+    {
+
+        $database = medoo::getInstance();
+
+        $database->openConnection(MYSQL_CONFIG);
+
+        $datos = $database->select('usuarios', '*', ["usuario[=]" => $usuario]);
+
+        $database->closeConnection();
+
+        return $datos;
+
+    }
+
+    public static function insertDB($data)
+
+    {
+
+        $database = medoo::getInstance();
+
+        $database->openConnection(MYSQL_CONFIG);
+
+        $datos = $database->insert('usuarios', [
+
+            "nombre" => $data['nombre'],
+
+            "usuario" => $data['usuario'],
+
+            "fecha_nac" => $data['fecha_nac'],
+
+            "apellidos" => $data['apellidos'],
+
+            "clave" => $data['clave']
+
+        ]);
+
+        $database->closeConnection();
+
+        return $datos;
+
+    }
+/*
+    public static function insertDB($data)
+
+    {
+
+        $database = medoo::getInstance();
+
+        $database->openConnection(MYSQL_CONFIG);
+
+        $datos = $database->insert('usuarios', $data);
+
+        $database->closeConnection();
+
+        return $datos;
+
+    }*/
+
+}
