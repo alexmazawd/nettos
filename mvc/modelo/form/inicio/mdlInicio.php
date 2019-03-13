0b6315e07466cdf9f3c9d5256945e5aa9509d497
alejoselopez <46781441+alejoselopez@users.noreply.github.com>
2019-03-13 15:39:22 +0100

    Create mdlInicio.php

---
+++ mvc/modelo/form/inicio/mdlInicio.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+class mdlInicio extends Singleton
+{
+    const PAGE = 'inicio';
+
+    public function onCargarVista($path)
+    {
+        if (getGet('pagina') != self::PAGE) return;
+
+        ob_start();
+
+        include $path;
+
+        $vista = ob_get_contents();
+
+        ob_end_clean();
+
+        echo $vista;
+    }
+}
