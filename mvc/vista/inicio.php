14c6336096478eed35fd3f0932d1c5d74aaa87bc
alejoselopez <46781441+alejoselopez@users.noreply.github.com>
2019-03-13 15:39:29 +0100

    Create inicio.php

---
+++ mvc/vista/inicio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Inicio</title>
+</head>
+<body>
+
+Hello World! <br>
+
+<a href="?pagina=edicion">Editar perfil</a>
+</body>
+</html>
