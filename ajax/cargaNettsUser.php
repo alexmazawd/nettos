e1731a543087dcc9fcfd741323dddc6e24e9fab0
alejoselopez <46781441+alejoselopez@users.noreply.github.com>
2019-05-15 18:03:59 +0200

    Update cargaNettsUser.php

--- ajax/cargaNettsUser.php
+++ ajax/cargaNettsUser.php
@@ -2,11 +2,35 @@
 
 require_once "../include.php";
 
+$netts=null;
+
+/*{
+    listaNetts: [
+    {
+    id_nett: "4",
+    id_usuario: "3",
+    contenido: "soy axl rose",
+    imagen: null,
+    fecha_pub: "2019-03-13 16:41:47"
+    },
+    {
+    id_nett: "5",
+    id_usuario: "3",
+    contenido: "axl y su segundo nett",
+    imagen: null,
+    fecha_pub: "2019-03-13 16:41:55"
+        }
+    ]
+}
+
+*/
+
+
 if (isset($_COOKIE['logged'])) { //Si esta cookie no existe no se ha iniciado sesion asi que el usuario es enviado al index
 
     $id = $_COOKIE['logged'];
 
-    $netts = netts::searchNettsUser($id);
+    $netts = netts::searchNettsUser($id);// para probar eliminar el if y colocar id de un usuario que haya hecho netts
 
 } else {
 
@@ -35,4 +59,4 @@ $objeto_json->listaNetts=$arrNetts;
 echo json_encode($objeto_json);
 
 //La funcion search Nett User devuelve un array asociativo
-// los arrays asociativos al codificarlos a json lo generan automaticamente
+//los arrays asociativos al codificarlos a json lo generan automaticamente
